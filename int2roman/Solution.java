3fa724718d8d1df37b47cdd4ff6d534c7bc0ea68
Yazid Hannachi <yazid.hannachi@tum.de>
2025-09-13 14:59:09 +0200

    fix int2roman

--- int2roman/Solution.java
+++ int2roman/Solution.java
@@ -2,36 +2,36 @@ package int2roman;
 
 import java.util.HashMap;
 import java.util.Map;
-// can be faster by adding the values of 4,9,40,90... to the map.(TODO)
+
 class Solution {
     public String intToRoman(int num) {
         String s= String.valueOf(num);
         StringBuilder result=new StringBuilder();
-        Map<Integer, Character> map = new HashMap<>();
-        map.put(1000, 'M');
-        map.put(500, 'D');
-        map.put(100, 'C');
-        map.put(50, 'L');
-        map.put(10, 'X');
-        map.put(5, 'V');
-        map.put(1, 'I');
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
         
         int n = (int) Math.pow(10, s.length()-1);
         while(num!=0){
             int x =(num/n);
             if ( x==4 ||x==9){
-                result.append(map.get(n));
-                if(x==4){
-                    result.append(map.get(5*n));
-                }
-                else{
-                    result.append(map.get(10*n));
-                }
+                result.append(map.get(n*x));
                 num-=x*n;
                 n/=10;
             }else{
                 if(x<4){
-                    char c=map.getOrDefault(n,'I');
+                    String c=map.getOrDefault(n,null);
                     for(int j=0;j<x;j++){
                         result.append(c);
                     }
@@ -39,7 +39,7 @@ class Solution {
                     n/=10;
                     
                 }else{
-                   char c=map.getOrDefault((5*n), 'E');
+                   String c=map.getOrDefault((5*n), null);
                    result.append(c);
                    num-=5*n;
                 }
