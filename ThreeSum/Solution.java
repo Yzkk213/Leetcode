567bef2a23ab6839614525adef0f24a5b19a4be4
Yazid Hannachi <yazid.hannachi@tum.de>
2025-09-14 19:47:30 +0200

    fixes

--- ThreeSum/Solution.java
+++ ThreeSum/Solution.java
@@ -2,9 +2,9 @@ package ThreeSum;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+
 import java.util.List;
-import java.util.Set;
+
 
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
